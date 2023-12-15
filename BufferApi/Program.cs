521bcc8805f84348baec4775e1a9383487c1dfdd
kukuFs <kurg5250@gmail.com>
2023-12-15 13:52:26 +0300

    Buffer v1.0.0.1
    1) Добавлен favicon
    2) Добавлено отображение версии
    3) Изменено некорректное отображение скролла

--- BufferApi/Program.cs
+++ BufferApi/Program.cs
@@ -1,6 +1,7 @@
 using BufferApi.Buffer;
 using BufferApi.DB;
 using BufferApi.Jobs;
+using BufferApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Quartz;
 using Quartz.Impl;
@@ -8,14 +9,10 @@ using Quartz.Spi;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//string rknetConnectingString = builder.Configuration.GetConnectionString("rknet");
-//builder.Services.AddDbContext<RknetContext>(options => options.UseSqlServer(rknetConnectingString));
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<CalculatorLogRepository>()
                 .AddSingleton<IJobFactory, SingletonJobFactory>()
@@ -25,31 +22,36 @@ builder.Services.AddSingleton<CalculatorLogRepository>()
                     jobType: typeof(BufferJob),
                     cronExpression: "0 */10 * ? * *"));
 builder.Services.AddHostedService<QuartzHostedService>();
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.UseStaticFiles();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.UseRouting();
+
+app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
         name: "default",
         pattern: "{controller=Home}/{action=Index}/{id?}");
-    // Дополнительный endpoint
+
+    // Дополнительные маршруты
     endpoints.MapControllerRoute(
         name: "customEndpoint",
         pattern: "custom/{controller=Home}/{action=Index}/{id?}");
 });
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseStaticFiles();
+
+app.MapControllers();
+
+
 
 
 app.Run();
